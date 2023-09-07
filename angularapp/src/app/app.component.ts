1cd82ce03bc5dfecf7d90ec229798ad0f1259d09
Iamneo <techadmin@iamneo.ai>
2023-09-07 08:22:45 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularapp';
+  userForm: FormGroup;
+  ngOnInit()
+  {
+    this.userForm=new FormGroup({
+      'inputFirstname':new FormControl(null,[Validators.required,Validators.maxLength(50),Validators.minLength(3),Validators.pattern('[a-z]*')]),
+      'gender':new FormControl(null,Validators.required),
+      'country':new FormControl(null,Validators.required),
+      'city':new FormControl(null,Validators.required),
+      'inputAge':new FormControl(null)
+    });
+  }
+  checkAgeValid(country:string,age:string){
+    if((country=='US' || country=='CA' || country=='IN' || country=='') && (age=='' || isNaN(parseInt(age))))
+    return true;
+    return false;
+  }
 }
